b01b20f0006fca6104ebd577a35404675d95f50b
janst <jan.starc@gmail.com>
2017-11-14 00:54:47 +0200

    Week 11 report semester project

--- app/src/main/java/com/jan/safealcohol/FirstActivity.java
+++ app/src/main/java/com/jan/safealcohol/FirstActivity.java
@@ -1,22 +1,16 @@
 package com.jan.safealcohol;
 
 import android.annotation.SuppressLint;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.Serializable;
@@ -26,47 +20,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-import static com.jan.safealcohol.FeedReaderContract.FeedEntry.COLUMN_NAME_AMOUNT;
-import static com.jan.safealcohol.FeedReaderContract.FeedEntry.COLUMN_NAME_NAME;
-import static com.jan.safealcohol.FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP;
-import static com.jan.safealcohol.FeedReaderContract.FeedEntry.COLUMN_NAME_UNITS;
-import static com.jan.safealcohol.FeedReaderContract.FeedEntry.TABLE_NAME;
-//import static com.jan.safealcohol.R.id.spinnerDesign;
 
-public class FirstActivity extends AppCompatActivity implements Serializable {
+public class FirstActivity extends AppCompatActivity  {
 
     private Button secondActivityButton;
     private Context context = this;
-
-    // TO DELETE
-    /*
-    private EditText amount;
-    private EditText percent;
-    private Button addButton;
-    private Spinner spinner;
-    */
-    FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(context);
-    // TO DELETE
-
-    // TO DELETE
-    //private EditText mealtime;
-    //private Button saveButton;
     private Button userDataButton;
     private TextView welcomeMessage;
     private Button addMealActivity;
 
-    // TO DELETE
 
-    // TO DELETE
     @SuppressLint("SimpleDateFormat")
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     SharedPreferences.Editor editor;
     SharedPreferences prefs;
     public static final String MY_PREFS_FILE = "MyPrefsFile";
-    private TextView unitsTextView;
+    private TextView textLevel;
+    private TextView textDrive;
     private Button updateUnitsButton;
-    private HashMap<String, Float> percentMap;
-    private HashMap<String, Float> amountMap;
     private HashMap<String, Float> levelMap;
     private DecimalFormat myFormat = new DecimalFormat("0.0");
     private Button addDrinkActivity;
@@ -77,16 +48,17 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstactivitydesign);
 
+
         // Create DB if not created
         new FeedReaderDbHelper(context);
         defineVariables();
-        percentMap = HashMaps.createPercentageMap();
-        amountMap = HashMaps.createAmountMap();
         levelMap = HashMaps.createLevelMap();
         callEventListeners();
-        //createDropdownMenu();
+        drawButtons();
         checkIfUserIsRegistered();
         updateUserMessages();
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         try {
             updateUnits((float) 0.0);
@@ -100,6 +72,7 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
         super.onResume();  // Always call the superclass method first
 
         Intent intent = getIntent();
+        drawButtons();
         String toastMessage = intent.getStringExtra("toast");
         if(toastMessage != null && !toastMessage.equals("")) {
             Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG).show();
@@ -117,22 +90,22 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
     public void defineVariables(){
 
         secondActivityButton = (Button) findViewById(R.id.secondActivity);
-        //amount = (EditText) findViewById(R.id.amountET);
-        //percent = (EditText) findViewById(R.id.percentET);
-        //addButton = (Button) findViewById(R.id.addButton);
-        //spinner = (Spinner) findViewById(spinnerDesign);
-
-
-
-        userDataButton = (Button) findViewById(R.id.userDataButton);
+        //userDataButton = (Button) findViewById(R.id.userDataButton);
         welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
-        unitsTextView = (TextView) findViewById(R.id.unitsDisplay);
-        updateUnitsButton = (Button) findViewById(R.id.updateUnitsButton);
+        textLevel = (TextView) findViewById(R.id.textLevel);
+        textDrive = (TextView) findViewById(R.id.textDrive);
+        //updateUnitsButton = (Button) findViewById(R.id.updateUnitsButton);
         addDrinkActivity = (Button) findViewById(R.id.addDrinkActivity);
         addMealActivity = (Button) findViewById(R.id.addMealActivity);
-        //mealTime = (TextView) findViewById(R.id.timeOfMeal);
-        lastMeal = (TextView) findViewById(R.id.lastMeal);
+        //lastMeal = (TextView) findViewById(R.id.lastMeal);
 
+    }
+
+    // Initialization of GUI at when the activity is started
+    public void drawButtons(){
+        secondActivityButton.setBackgroundResource(R.drawable.drinking_history_default);
+        addDrinkActivity.setBackgroundResource(R.drawable.add_drink_home_default);
+        addMealActivity.setBackgroundResource(R.drawable.add_meal_home_default);
     }
 
     /**
@@ -141,26 +114,10 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
 
     public void callEventListeners(){
         secondActivityButton.setOnClickListener(startSecondActivityListener);
-        //addButton.setOnClickListener(addDrinkListener);
-
-        userDataButton.setOnClickListener(startUserDataListener);
-        updateUnitsButton.setOnClickListener(updateUnitsListener);
+        //userDataButton.setOnClickListener(startUserDataListener);
+        //updateUnitsButton.setOnClickListener(updateUnitsListener);
         addDrinkActivity.setOnClickListener(addDrinkActivityListener);
         addMealActivity.setOnClickListener(addMealActivityListener);
-
-        /*
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                fillAmountPercent();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
-            }
-        });
-        */
     }
 
     // Starts UserDataActivity
@@ -172,41 +129,14 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
         }
     };
 
-
-
-    /*
-    // Adds new drink to DB
-    View.OnClickListener addDrinkListener = new Button.OnClickListener() {
-
-        @Override
-        public void onClick(View v){
-            try {
-                addDrinkToDB();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-    };
-    */
-
     // Starts Drink History activity
     View.OnClickListener startSecondActivityListener = new Button.OnClickListener() {
 
         @Override
         public void onClick(View v){
+
+            secondActivityButton.setBackgroundResource(R.drawable.drinking_history_pressed);
             runSecondActivity();
-        }
-    };
-
-    View.OnClickListener updateUnitsListener = new Button.OnClickListener(){
-
-        @Override
-        public void onClick(View v){
-            try {
-                updateUnits((float) 0.0);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
         }
     };
 
@@ -214,6 +144,8 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
 
         @Override
         public void onClick(View v){
+
+            addDrinkActivity.setBackgroundResource(R.drawable.add_drink_home_pressed);
             runAddDrinkActivity();
         }
     };
@@ -222,6 +154,8 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
 
         @Override
         public void onClick(View v){
+
+            addMealActivity.setBackgroundResource(R.drawable.add_meal_home_pressed);
             runAddMealActivity();
         }
     };
@@ -251,66 +185,6 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
 
         }
     }
-
-    public void fillAmountPercent(){
-        /*
-        String item = spinner.getSelectedItem().toString();
-        percent.setText(percentMap.get(item).toString());
-        amount.setText(amountMap.get(item).toString());
-        */
-    }
-
-    /*
-    public void createDropdownMenu(){
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.drinks_array, android.R.layout.simple_spinner_item);    // Create an ArrayAdapter using the string array and a default spinner layout
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);             // Specify the layout to use when the list of choices appears
-        spinner.setAdapter(adapter);                // Apply the adapter to the spinner
-    }
-    */
-
-    /*
-    public void addDrinkToDB() throws ParseException {
-
-        String amountText = amount.getText().toString();
-        String percentText = percent.getText().toString();
-        if(amountText.equals("") || percentText.equals("")){
-            Toast.makeText(getApplicationContext(), "Please fill all the fields", Toast.LENGTH_LONG).show();
-        } else if (Float.valueOf(amountText) > 10.1f || Float.valueOf(amountText) < 0.1f) {
-            Toast.makeText(getApplicationContext(), "Please check the amount again", Toast.LENGTH_LONG).show();
-        } else if (Float.valueOf(percentText) > 100 || Float.valueOf(percentText) < 0){
-            Toast.makeText(getApplicationContext(), "Please check the percentage again", Toast.LENGTH_LONG).show();
-        } else {
-
-            float amount = Float.valueOf(amountText);
-
-            // Gets the data repository in write mode
-            SQLiteDatabase db = mDbHelper.getWritableDatabase();
-            String selectedDrink = spinner.getSelectedItem().toString();
-            float percent = percentMap.get(selectedDrink);
-            if(Float.valueOf(percentText) != percent) percent = Float.valueOf(percentText);
-            Float newDrinkUnits = (amount*(percent)/100) / 0.125f;
-            Log.d("drinkCalc", "newDrinkUnits: " + newDrinkUnits);
-
-            // Create a new map of values, where column names are the keys
-            ContentValues values = new ContentValues();
-            values.put(COLUMN_NAME_NAME, selectedDrink);
-            values.put(COLUMN_NAME_AMOUNT, amount);
-            values.put(COLUMN_NAME_UNITS, newDrinkUnits);                        // TODO To add the units - new DB for drinks
-
-            updateUnits(newDrinkUnits);                                         // Recalculate the units, put on screen, update SharedPref file!
-
-
-            // Custom timestamp
-            Date myDate = new Date();
-            String date = dateFormat.format(myDate);
-            values.put(COLUMN_NAME_TIMESTAMP, date);
-
-            // Insert the new row, returning the primary key value of the new row
-            long newRowId = db.insert(TABLE_NAME, null, values);
-        }
-    }
-    */
 
     /**
      * 1.) Ko se drink doda, se level alkohola itak dvigne za vrednost dodatka
@@ -355,13 +229,32 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
 
         Float currentUnits = prefs.getFloat("units", (float) 0.0);
         Float alcoLevel = prefs.getFloat("alcoLevel", (float) 0.0);
-        unitsTextView.setText("Current units: " + myFormat.format(currentUnits) + "  |  AlcoLevel: " + myFormat.format(alcoLevel) + "\n");
-        String limitInCountry = prefs.getString("country", null);
 
-        if(limitInCountry != null){
-            Log.d("limitString", "Here1");
-            Log.d("limitString", "Level: " + myFormat.format(levelMap.get(prefs.getString("country", null)) * 10));
-            unitsTextView.append("Limit in your country: " + myFormat.format(levelMap.get(prefs.getString("country", null)) * 10));
+        Float aboveLimit = 0f;
+        textLevel.setText("Current units: " + myFormat.format(currentUnits) + "  |  AlcoLevel: " + myFormat.format(alcoLevel) + "\n");
+        String country = prefs.getString("country", null);
+        Float limitInCountry = levelMap.get(country)*10;
+        Log.d("calc", "Limit in country: " + limitInCountry + "AlcoLevel: " + alcoLevel);
+        Log.d("calc", "Calc: " + (alcoLevel-limitInCountry));
+
+        if(limitInCountry != -1){
+            //Log.d("limitString", "Alco level: " + alcoLevel);
+            //Log.d("limitString", "Level: " + myFormat.format(levelMap.get(prefs.getFloat("country", -1)) * 10));
+
+
+            aboveLimit = alcoLevel - limitInCountry;
+            if(aboveLimit < 0){
+                float out = Math.abs(aboveLimit);
+                textDrive.setText("You are " + myFormat.format(out) + " below the limit. Be careful!\n");
+            } else if (aboveLimit == 0){
+                textDrive.setText("You are on limit. Remember, app just calculates an assumption. Wait before you drive");
+            } else {
+                textDrive.setText("You are " + myFormat.format(aboveLimit) + " above the limit. Don't even think about driving!");
+            }
+
+            textDrive.append("Limit in your country: " + myFormat.format(levelMap.get(prefs.getString("country", null)) * 10));
+        } else {
+            textDrive.setText("Limit for your country is unknown!");
         }
     }
 
@@ -395,26 +288,9 @@ public class FirstActivity extends AppCompatActivity implements Serializable {
         prefs = getSharedPreferences(MY_PREFS_FILE, MODE_PRIVATE);
         String fn = prefs.getString("firstname", null);
         String ln = prefs.getString("lastname", null);
-        welcomeMessage.setText("Welcome, " + fn + " " + ln);
-        int sizeOfMeal = prefs.getInt("sizeofmeal", 1);
-        String mealType = "";
-
-        switch(sizeOfMeal) {
-            case 1:
-                mealType = "snack";
-                break;
-            case 2:
-                mealType = "mid-size meal";
-                break;
-            case 3:
-                mealType = "full meal";
-                break;
-        }
-
-        String mt = prefs.getString("timeofmeal", null);
-        Date myDate = new Date();
-        String date = dateFormat.format(myDate);
-        lastMeal.setText("Your last meal was a " + mealType + " on " + mt);
+        welcomeMessage.setText(fn + " " + ln);
+        
+        
     }
 
     public long calculateTimeDifference(Date date1, Date date2){
